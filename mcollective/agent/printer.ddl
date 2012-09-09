c3b3a2cfd3d58d12b5463975b0675be60d250e69
Gary Larizza <gary@puppetlabs.com>
2012-09-09 16:40:54 -0700

    Refactor printer agent
    I wrote the printer agent back before I really understood how to use
    run() and how MCollective Agents worked (I don't profess complete
    knowledge; just a tiny bit BETTER knowledge at this point ;). This
    commit uses run() where possible to shell out. There's still room to
    locate the lp commands and catch when they don't exist - that should
    be the next refactor.

--- mcollective/agent/printer.ddl
+++ mcollective/agent/printer.ddl
@@ -17,17 +17,62 @@ end
 action "add", :description => "Adds a printer to the system" do
   display :always
 
-  output :output,
-         :description => "Add a printer",
-         :display_as  => "Printer"
+  output :add_output,
+         :description => "Add a printer output",
+         :display_as  => "Output from Printer Creation"
+
+  output :add_errors,
+         :description => "Errors from creating a printer",
+         :display_as  => "Errors from Printer Creation"
+
+  input :printer,
+        :prompt      => "Printer name (NO SPACES)",
+        :description => "Printer name (NO SPACES)",
+        :optional    => false,
+        :type        => :string,
+        :validation  => '(.*?)',
+        :maxlength   => 230
+
+  input :address,
+        :description => "Printer address (in the format of 'lpd://10.13.1.10')",
+        :prompt      => "Printer address (in the format of 'lpd://10.13.1.10')",
+        :optional    => false,
+        :type        => :string,
+        :validation  => '(.*?)',
+        :maxlength   => 230
+
+  input :location,
+        :description => "Printer location (Please quote value if it contains spaces)",
+        :prompt      => "Printer location (Please quote value if it contains spaces)",
+        :optional    => true,
+        :type        => :string,
+        :validation  => '(.*?)',
+        :maxlength   => 230
+
+  input :driver,
+        :description => "Printer driver file (Please escape or quote values with spaces)",
+        :prompt      => "Printer driver file (Please escape or quote values with spaces)",
+        :optional    => true,
+        :type        => :string,
+        :validation  => '(.*?)',
+        :maxlength   => 230
 end
 
 action "remove", :description => "Removes specified printers" do
   display :always
 
   output :output,
-         :description => "Remove Printers",
-         :display_as  => "Removed Printers"
+         :description => "Output",
+         :display_as  => "Output"
+  
+  input :printer,
+        :prompt      => "Printer name (NO SPACES)",
+        :description => "Printer name (NO SPACES)",
+        :optional    => false,
+        :type        => :string,
+        :validation  => '(.*?)',
+        :maxlength   => 230
+
 end
 
 action "devices", :description => "List printers and their devices" do
@@ -42,14 +87,27 @@ action "cancel", :description => "Cancels print jobs for a specified queue" do
   display :always
 
   output :output,
-         :description => "Cancelled Print Jobs",
-         :display_as  => "Cancelled Jobs"
+         :description => "Output",
+         :display_as  => "Output"
+  
+  input :printer,
+        :prompt      => "Printer name (NO SPACES)",
+        :description => "Printer name (NO SPACES)",
+        :optional    => false,
+        :type        => :string,
+        :validation  => '(.*?)',
+        :maxlength   => 230
+
 end
 
 action "cancel_all", :description => "Cancels ALL print jobs for ALL printers" do
   display :always
 
   output :output,
-         :description => "Cancelled Print Jobs",
-         :display_as  => "Cancelled Jobs"
+         :description => "Output",
+         :display_as  => "Output"
+  
+  output :errors,
+         :description => "Errors",
+         :display_as  => "Errors"
 end
--- mcollective/agent/printer.rb
+++ mcollective/agent/printer.rb
@@ -10,33 +10,50 @@ module MCollective
                :timeout     => 100
 
       action 'list' do
-        reply[:output] = `lpstat -a | cut -d ' ' -f 1`.split("\n")
+        list = []
+        run("lpstat -a | cut -d ' ' -f 1", :stdout => list, :stderr => :errors)
+
+        if list.first
+          reply[:output] = list.first.split
+        else
+          reply[:output] = 'No printers have been installed'
+        end
       end
 
       action 'devices' do
-	      reply[:output] = `lpstat -v | cut -d ' ' -f 3,4`.split("\n")
+        list = []
+	      run("lpstat -v | cut -d ' ' -f 3,4", :stdout => list, :stderr => :errors)
+        reply[:output] = list
       end
 
       action 'add' do
         validate :printer, :shellsafe
         validate :address, :shellsafe
 
-        printerarray = `lpstat -a | cut -d ' ' -f 1`
-        reply.fail! "Printer already installed on the system." if printerarray.include? request[:printer] }
+        list = []
+        run("lpstat -a | cut -d ' ' -f 1", :stdout => list, :stderr => :errors)
+        printerarray = list.first.split
 
-        args = ['lpadmin ']
-        args.push "-p #{request[:printer]} " if request[:printer]
-        args.push "-v #{request[:address]} " if request[:address]
+        reply.fail! "Printer already installed on the system." if printerarray.include? request[:printer]
+
+        args = 'lpadmin '
+        args << "-p #{request[:printer]} " if request[:printer]
+        args << "-v #{request[:address]} " if request[:address]
 
         validate :location, :shellsafe and
-          args.push "-L #{request[:location]} " if request[:location]
+          args << "-L #{request[:location]} " if request[:location]
         validate :driver, :shellsafe and
-          args.push "-P #{request[:driver]} " if request[:driver]
-        args.push "-E "
+          args << "-P #{request[:driver]} " if request[:driver]
+        args << "-E "
 
-        `#{args}`
+        # Run the command created from above
+        run(args, :stdout => :add_output, :stderr => :add_errors)
 
-        printerarray = `lpstat -a | cut -d ' ' -f 1`.split("\n")
+        # Update the list of printers
+        list = []
+        run("lpstat -a | cut -d ' ' -f 1", :stdout => list)
+        printerarray = list.first.split
+
         if printerarray.include? request[:printer]
           reply[:output] = "Printer #{request[:printer]} successfully installed on the system."
         else
@@ -48,31 +65,39 @@ module MCollective
       action 'remove' do
         validate :printer, :shellsafe
 
-        return_code = system("lpadmin -x #{request[:printer]}")
+        status = run("lpadmin -x #{request[:printer]}")
 
-        if return_code
+        if status == 0
           reply[:output] = "Successfully removed printer: #{request[:printer]}\n"
         else
+          reply[:output] = "There was an error attempting to delete printer: #{request[:printer]}"
 	        reply.fail! "There was an error attempting to delete printer: #{request[:printer]}"
         end
       end
 
       action 'cancel' do
         validate :printer, :shellsafe
+        list = []
 
-        jobsarray = `lpstat -P #{request[:printer]}`
+        run("lpstat -P #{request[:printer]}", :stdout => list)
 
-        if jobsarray == ""
-          reply.fail! "There are no print jobs for that printer."
+        if list.empty?
+          reply[:output] = "There are no print jobs for that printer."
         else
           reply[:output] = "Cancelling all print jobs for the #{request[:printer]} printer."
-          `lprm -P #{request[:printer]} -`
+          run("lprm -P #{request[:printer]} -")
         end
       end
 
       action 'cancel_all' do
-        `cancel -a -`
-        reply[:output] = "Cancelled all print jobs on all queues"
+        errors = []
+        run('cancel -a -', :stderr => errors)
+
+        unless errors.empty?
+          reply[:errors] = errors.first
+        else
+          reply[:output] = "Cancelled all print jobs on all queues"
+        end
       end
     end
   end
